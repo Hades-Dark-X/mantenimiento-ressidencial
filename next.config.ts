36dbbd79a6af101777974f79a38fb6d4868f560e
Hades-Dark-X <marcosgonzalezcahavez09@gmail.com>
2025-09-05 17:12:47 -0600

    Actualizacion del archivo next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/mantenimiento-ressidencial",
+  images: {
+    path: "/mantenimiento-ressidencial/_next/image",
+  },
 };
 
 export default nextConfig;
